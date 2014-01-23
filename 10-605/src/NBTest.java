ca01e6b08f07ad6e1805677797e5b69346c354ce
JerrySun363 <sunchen363@gmail.com>
2014-01-23 14:11:44 -0500

    initial code OK

---
+++ 10-605/src/NBTest.java
@@ -0,0 +1,107 @@
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Vector;
+
+
+public class NBTest {
+	
+	public static HashMap<String, Integer> labelCount = new HashMap<String, Integer>();
+	public static HashMap<String,HashMap<String, Integer>> labelWordCount = new HashMap<String,HashMap<String, Integer>>();
+	
+	public static void main(String args[]) throws IOException{
+		BufferedReader bi = new BufferedReader(new InputStreamReader(System.in));
+		String line;
+		/**
+		 * Construct the data from below
+		 */
+		while ((line = bi.readLine()) != null){
+			String[] tokens= line.split("\t");
+			String label = tokens[0];
+			String countString = tokens[1];
+			int count = Integer.parseInt(countString);
+			parseLabel(label,count);
+			readTest(args[0]);
+		}
+	}
+
+	private static void parseLabel(String label,int count) {
+		
+		if(!label.contains(",")){
+			String[] Y = label.split("=");
+			labelCount.put(Y[1], count);
+		}else{
+			String[] parts = label.split(",");
+			String[] Y=parts[0].split("=");
+			String[] W=parts[1].split("=");
+			if(labelWordCount.containsKey(Y[1])){
+				HashMap<String, Integer> wordCount = labelWordCount.get(Y[1]);
+				wordCount.put(W[1], count);
+			}else{
+				HashMap<String, Integer> wordCount = new HashMap<String, Integer>();
+				wordCount.put(W[1], count);
+				labelWordCount.put(Y[1], wordCount);
+			}
+				
+		}
+		
+	}
+
+	private static void readTest(String test){
+		try {
+			BufferedReader bi = new BufferedReader(new InputStreamReader(new FileInputStream(test)));
+			String line;
+			while((line=bi.readLine())!=null){
+				String[] parts = line.split("\t");
+				//String[] labels = parts[0].split(",");
+				String cur_doc = parts[1].trim();
+				Vector<String> words = NBTrain.tokenizeDoc(cur_doc);
+				String result = makePredicition(words);
+				System.out.println(result);
+				
+			}
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private static String makePredicition(Vector<String> words) {
+		double maxPro = Double.MIN_VALUE;
+		String maxP ="";
+		int totalCount = labelCount.get("*");
+		
+		for(String label :labelCount.keySet()){
+			if(label.equals("."))
+				continue;
+			int article = labelCount.get(label);
+			double p = Math.log((article+1)*1.0/(totalCount+4));
+			HashMap<String, Integer> dic = labelWordCount.get(label);
+			int dicSize = dic.get("*");
+			dicSize+=dic.size()-1;
+			
+			for(String word : words){
+				int count =1;
+				if(word.equals("*"))
+					continue;
+				
+				if(dic.containsKey(word))
+					count+=dic.get(word);
+				
+				p += Math.log(count*1.0/dicSize);
+				
+			}
+			
+			if(p>maxPro){
+				maxPro = p;
+				maxP = label;
+			}
+		}
+		
+		return maxP+"\t"+maxPro;
+	}
+}
---
+++ 10-605/src/NBTrain.java
@@ -0,0 +1,76 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Set;
+import java.util.Vector;
+
+
+public class NBTrain {
+	public static void main(String args[]) throws IOException{
+		BufferedReader bi = new BufferedReader(new InputStreamReader(System.in));
+		String line;
+		HashMap<String, Integer> labelCount = new HashMap<String, Integer>();
+		HashMap<String, Integer> labelWordCount = new HashMap<String, Integer>();
+		int totalCount=0;
+		while ((line = bi.readLine()) != null){
+			String[] parts = line.split("\t");
+			String[] labels = parts[0].split(",");
+			String cur_doc = parts[1].trim();
+			Vector<String> words = tokenizeDoc(cur_doc);
+			
+			for(String label:labels){
+				//only four is considered at the moment.
+				if(!label.equals("CCAT") && !label.equals("ECAT") && !label.equals("GCAT") && !label.equals("MCAT"))
+					continue;
+				
+				String newLabel = "Y="+label;
+				totalCount++;
+				if(labelCount.containsKey(newLabel)){
+					int count = labelCount.get(newLabel);
+					labelCount.put(newLabel, count+1);
+				}else{
+					labelCount.put(newLabel, 1);
+				}
+				for(String word:words){
+					String key = newLabel+",W="+word;
+					if(labelWordCount.containsKey(key)){
+						labelWordCount.put(key, labelWordCount.get(key)+1);
+					}else{
+						labelWordCount.put(key, 1);
+					}
+				}
+				String allWord = "Y="+label+",W=*";
+				if(labelWordCount.containsKey(allWord)){
+					labelWordCount.put(allWord, labelWordCount.get(allWord)+1);
+				}else{
+					labelWordCount.put(allWord,1);
+				}
+			}
+		}
+		System.out.println("Y=*\t"+totalCount);
+		printHashMap(labelCount);
+		printHashMap(labelWordCount);
+		   
+	}
+	
+	static Vector<String> tokenizeDoc(String cur_doc) {
+        String[] words = cur_doc.split("\\s+");
+        Vector<String> tokens = new Vector<String>();
+        for (int i = 0; i < words.length; i++) {
+        	words[i] = words[i].replaceAll("\\W", "");
+        	if (words[i].length() > 0) {
+        		tokens.add(words[i]);
+        	} 
+        }
+		return tokens;
+	}
+	
+	static void printHashMap(HashMap<String, Integer> count){
+		Set<String> keys = count.keySet();
+		for(String s:keys){
+			System.out.println(s+"\t"+count.get(s));
+		}
+	}
+	
+}
